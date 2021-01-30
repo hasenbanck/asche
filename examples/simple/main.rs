96c66be97b4e09ca283b0f37e90a2fe9d0be75cc
Nils Hasenbanck <nils@hasenbanck.de>
2021-01-30 11:28:58 +0100

    Use SDL2 in the example to try to reduce upstream dependencies.

--- examples/simple/main.rs
+++ examples/simple/main.rs
@@ -1,11 +1,14 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use raw_window_handle::HasRawWindowHandle;
 
-use asche::DeviceDescriptor;
-
 fn main() -> Result<()> {
-    let eventloop = winit::event_loop::EventLoop::new();
-    let window = winit::window::Window::new(&eventloop)?;
+    let sdl_context = sdl2::init().map_err(|s| anyhow!(s))?;
+    let video_subsystem = sdl_context.video().map_err(|s| anyhow!(s))?;
+
+    let window = video_subsystem
+        .window("simple example", 800, 600)
+        .vulkan()
+        .build()?;
 
     // Log level is based on RUST_LOG env var.
     tracing_subscriber::fmt::init();
@@ -18,7 +21,7 @@ fn main() -> Result<()> {
             vulkan_version: asche::VulkanVersion::V1,
         },
     )?;
-    let _device = instance.request_device(&DeviceDescriptor {
+    let _device = instance.request_device(&asche::DeviceDescriptor {
         ..Default::default()
     })?;
     Ok(())
